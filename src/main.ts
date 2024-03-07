fe595b8660e2ec85c41ee2357fba86acdf73d152
暖阳 <532764597@qq.com>
2024-03-07 23:21:41 +0800

    add elementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+// 导入全局css
 import './assets/main.css'
+import 'element-plus/dist/index.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import  ElementPlus  from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +12,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ElementPlus)
 app.use(router)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
   <div class="about">
+    <el-button>我是 ElButton</el-button>
     <h1>This is an about page</h1>
   </div>
 </template>
-
 <style>
 @media (min-width: 1024px) {
   .about {
